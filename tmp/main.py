3c25ddffd7b3e47dfc2ee0e511666d265425cb4b
PashtetS0 <icerain0@gmail.com>
2023-08-02 20:41:01 +0300

    Раскомментил для работы без заранее созданного json

--- tmp/main.py
+++ tmp/main.py
@@ -4,12 +4,12 @@ import bd
 from modules import bot
 
 if __name__ == '__main__':
-    # result_query = bot.query()  # Выполняем запрос - хочет человек познакомиться с кем-нибудь, или нет:
-    # result_search_data = bot.requesting_search_data(result_query)  # Запрос данных для поиска по параметрам:
+    result_query = bot.query()  # Выполняем запрос - хочет человек познакомиться с кем-нибудь, или нет:
+    result_search_data = bot.requesting_search_data(result_query)  # Запрос данных для поиска по параметрам:
             # Конечный вывод:
             # found = bot.create_found(result_search_data)
             # pprint(found)
-    # bot.create_found(result_search_data)  # Создается json для дальнейшей работы
+    bot.create_found(result_search_data)  # Создается json для дальнейшей работы
     with open('found.json', 'r', encoding='utf-8') as file:
         data = json.load(file)
     # pprint(data)
@@ -17,4 +17,3 @@ if __name__ == '__main__':
 
     bd.create_tables(bd.engine)
     bd.session.close()
-
