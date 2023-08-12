df7c0a405a17355338cae4c73b49c9e06525501b
PashtetS0 <icerain0@gmail.com>
2023-08-12 20:47:36 +0300

    Bot_3

---
+++ Bot_3/db/database.py
@@ -0,0 +1,96 @@
+import sqlalchemy as sq
+import json
+# from sqlalchemy.orm import declarative_base, relationship, sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
+from db.models import User, Favorite, BlackList, create_tables
+def create_connection(user_name, password, host_name, port, db_name):
+    DSN = f'postgresql://{user_name}:{password}@{host_name}:{port}/{db_name}'
+    return DSN
+
+class Create:
+    def __init__(self):
+        pass
+    def add_data_to_db(self, data):
+        for record in data:
+            session.add(User(vk_id=record.get('id'),
+                        f_name=record.get('first_name'),
+                        l_name=record.get('last_name'),
+                        profile_link=record.get('link'),
+                        hometown=record.get('home_town'),
+                        photo1=record.get('photos')[0]['url'],
+                        photo2=record.get('photos')[1]['url'],
+                        photo3=record.get('photos')[2]['url'],
+            )
+        )
+
+    def add_data_to_favorite(self, data):
+        print(data)
+        for record in data:
+            print(record)
+            session.add(Favorite(vk_id=record.get('id'),
+                        profile_link=record.get('link'),
+                        photo=record.get('photo')
+            )
+        )
+
+    def add_data_to_black(self, data):
+        print(data)
+        for record in data:
+            print(record)
+            session.add(BlackList(vk_id=record.get('id'),
+                        profile_link=record.get('link'),
+                        photo=record.get('photo')
+            )
+        )
+
+def get_data_json(route):
+    with open(route, 'r', encoding="utf-8") as f:
+        data = json.load(f)
+    return data
+
+class Read:
+    def __init__(self):
+        pass
+    def read_from_db_users(self):
+        q = session.query(User.f_name, User.l_name, User.profile_link, User.photo1, User.photo2, User.photo3)
+        return q.all()
+    def read_from_db_favorite(self):
+        q = session.query(Favorite.vk_id, Favorite.profile_link, Favorite.photo)
+        return q.all()
+    def read_from_db_black(self):
+        q = session.query(BlackList.vk_id, BlackList.profile_link, BlackList.photo)
+        return q.all()
+
+Base = declarative_base()
+DSN = create_connection('postgres', '1', 'localhost', 5432, 'VKinder')
+engine = sq.create_engine(DSN)
+create_tables(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def users_info_for_bot():
+    data_found = get_data_json('found.json')
+    create_instance = Create()
+    create_instance.add_data_to_db(data_found)
+    session.commit()
+    read_db_instance = Read()
+    return read_db_instance.read_from_db_users()
+
+def favorite_info_for_bot():
+    data_favorite = get_data_json('favorites.json')
+    create_instance = Create()
+    create_instance.add_data_to_favorite(data_favorite)
+    session.commit()
+    read_db_instance = Read()
+    return read_db_instance.read_from_db_favorite()
+
+def black_info_for_bot():
+    data_black = get_data_json('black.json')
+    print(data_black)
+    create_instance = Create()
+    create_instance.add_data_to_black(data_black)
+    session.commit()
+    read_db_instance = Read()
+    return read_db_instance.read_from_db_black()
+
+session.close()
